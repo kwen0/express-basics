1d30b723334424520459991a1717185828047bae
Karen <karen.wen3@gmail.com>
2022-03-18 12:29:22 -0400

    handle get single member req

--- index.js
+++ index.js
@@ -3,14 +3,23 @@ const app = express()
 const path = require('path')
 const members = require('./Members')
 const logger = require('./middleware/logger')
-const moment = require('moment')
 
 // Init middleware
-app.use(logger)
+// app.use(logger)
 
 // Gets all members
 app.get('/api/members', (req, res) => {
     res.json(members);
+})
+
+// Get single member
+app.get('/api/members/:id', (req, res) => {
+    const found = members.some(member => member.id === parseInt(req.params.id))
+    if (found) {
+        res.json(members.filter(member => member.id === parseInt(req.params.id)))
+    } else {
+        res.status(400).json({ msg: `No member with the id of ${req.params.id}` })
+    }
 })
 
 // Set static folder
