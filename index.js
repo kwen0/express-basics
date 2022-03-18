0e281761b24c629c99da4708453981d089371cee
Karen <karen.wen3@gmail.com>
2022-03-18 12:20:38 -0400

    initialize middleware

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
 const express = require('express')
 const app = express()
 const path = require('path')
+const members = require('./Members')
+const logger = require('./middleware/logger')
+const moment = require('moment')
+
+// Init middleware
+app.use(logger)
+
+// Gets all members
+app.get('/api/members', (req, res) => {
+    res.json(members);
+})
 
 // Set static folder
 app.use(express.static(path.join(__dirname, 'public')))
